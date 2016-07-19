9783f575fb76f599d426fa6e217ee96446011f7f
Ben Coleman <colemabs@miamioh.edu>
2016-07-18 21:22:22 -0400

    with hover functionality

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
 
 function genDivs(){
-var height = prompt("How many blocks in your height?");
-var width = prompt("How many blocks in your width?");
+var boxSize = prompt("What would you like the size of your box to be??");
 var $container = $('#container');
 var $row = $('.row')
 	
 
-	for(i = 0; i < height; i++){
+	for(i = 0; i < boxSize; i++){
 		$container.append("<div class='row'></div>").addClass(function( i ){
 			return 'row' + i;
 		});
 
-
-		for(x = 0; x < width; x++ ){
+		for(x = 0; x < boxSize; x++ ){
 			
 			$container.append("<div class='cell'></div>");
 		}
 	}
-	
 
+} //end of genDivs()
+
+function hoverCell(){
+	$('.cell').on('mouseenter',function(){
+		$(this).css('background', 'red');
+	});
 }
+
 genDivs();
+hoverCell();
 });
